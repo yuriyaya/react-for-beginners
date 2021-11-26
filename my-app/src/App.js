e43477f83fd3af0c50151a3423b02fe4fa7e1939
yuriya <namyuri@gmail.com>
2021-11-26 22:02:52 +0900

    chapter 6.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back</h1>
-      <Button text={"test button"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>count up</button>
     </div>
   );
 }
